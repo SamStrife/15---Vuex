b2f665a3cb08b14fe1004e87e248ffa67725121d
Sam <sam.a.fletcher@gmail.com>
2022-01-16 13:26:28 +0000

    Did the lessons

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex" v-if="!isAuth">
+    <the-coutner></the-coutner>
+    <favorite-value></favorite-value>
+    <button @click="addOne">Add 10</button>
+    <change-counter></change-counter>
+  </base-container>
+  <base-container title="auth">
+    <user-auth></user-auth>
+  </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCoutner from './components/TheCoutner.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
+import UserAuth from './components/UserAuth.vue';
 
 export default {
   components: {
     BaseContainer,
+    TheCoutner,
+    ChangeCounter,
+    FavoriteValue,
+    UserAuth,
+  },
+  computed: {
+    isAuth() {
+      return this.$store.getters.userIsAuthenticated;
+    },
+  },
+  methods: {
+    addOne() {
+      this.$store.dispatch('increase', { value: 10 });
+    },
   },
 };
 </script>
